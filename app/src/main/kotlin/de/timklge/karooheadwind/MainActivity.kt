2e5b0d6dbdcf8cf5868c909fb114b2412aa279d1
timklge <2026103+timklge@users.noreply.github.com>
2025-04-17 18:12:43 +0200

    Reset preferences if file is corrupted (#92)

--- app/src/main/kotlin/de/timklge/karooheadwind/MainActivity.kt
+++ app/src/main/kotlin/de/timklge/karooheadwind/MainActivity.kt
@@ -1,16 +1,10 @@
 package de.timklge.karooheadwind
 
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.preferencesDataStore
 import de.timklge.karooheadwind.screens.MainScreen
 import de.timklge.karooheadwind.theme.AppTheme
-
-val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
