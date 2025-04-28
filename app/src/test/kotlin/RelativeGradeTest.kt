e5675794824224eab1b5159c7c0f6a57b1eeb08a
Tim Kluge <timklge@gmail.com>
2025-04-28 18:35:28 +0200

    Add additional relative grade estimation tests

--- app/src/test/kotlin/RelativeGradeTest.kt
+++ app/src/test/kotlin/RelativeGradeTest.kt
@@ -17,6 +17,32 @@ class RelativeGradeTest {
     }
 
     @Test
+    fun testStrongHeadwind() {
+        val grade1 = RelativeGradeDataType.estimateRelativeGrade(
+            actualGrade = 0.02, // 2%
+            riderSpeed = 8.0,   // m/s (~28.8 km/h)
+            windSpeed = 10.0,   // m/s (36 km/h)
+            windDirectionDegrees = 0.0, // Direct headwind
+            totalMass = 80.0    // kg
+        )
+
+        assertEquals(0.086, grade1, 0.01) // Expected relative grade is approximately 8.6%
+    }
+
+    @Test
+    fun testDiagonalHeadwind() {
+        val grade1 = RelativeGradeDataType.estimateRelativeGrade(
+            actualGrade = 0.02, // 2%
+            riderSpeed = 8.0,   // m/s (~28.8 km/h)
+            windSpeed = 5.0,    // m/s (18 km/h)
+            windDirectionDegrees = 45.0, // Diagonal headwind
+            totalMass = 80.0    // kg
+        )
+
+        assertEquals(0.037, grade1, 0.01) // Expected relative grade is approximately 3.7%
+    }
+
+    @Test
     fun testTailwind(){
         val grade2 = RelativeGradeDataType.estimateRelativeGrade(
             actualGrade = 0.02, // 2%
