f4c3261a656b9de4be93c81ca32be3f5b1cd9b1f
Tim Kluge <timklge@gmail.com>
2025-08-15 21:22:04 +0200

    Fix plain wind gust / wind speed datafields show values in meters per second

--- app/src/main/kotlin/de/timklge/karooheadwind/datatypes/WindGustsDataType.kt
+++ app/src/main/kotlin/de/timklge/karooheadwind/datatypes/WindGustsDataType.kt
@@ -3,10 +3,15 @@ package de.timklge.karooheadwind.datatypes
 import android.content.Context
 import de.timklge.karooheadwind.weatherprovider.WeatherData
 import io.hammerhead.karooext.KarooSystemService
+import io.hammerhead.karooext.models.DataType
 import io.hammerhead.karooext.models.UserProfile
 
 class WindGustsDataType(karooSystemService: KarooSystemService, context: Context) : BaseDataType(karooSystemService, context, "windGusts"){
     override fun getValue(data: WeatherData, userProfile: UserProfile): Double {
         return data.windGusts
+    }
+
+    override fun getFormatDataType(): String {
+        return DataType.Type.SPEED
     }
 }
